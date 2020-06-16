b591720cc2b4dbf4efa6df08320bc4868278d1af
xiongwei <2894001817@qq.com>
2020-06-16 19:47:26 +0800

    初始化项目

---
+++ src/test/java/com/twschool/practice/HelloTest.java
@@ -0,0 +1,12 @@
+package com.twschool.practice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void should_run_test_pass() {
+        Assert.assertTrue(true);
+    }
+}
